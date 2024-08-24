0e984915ab93bd6d181147298d9b88b1fcbcb3d8
manoela <manoelamartedi@hotmail.com>
2024-08-24 16:30:02 -0300

    exercicios-1-2

---
+++ heranca-exercicios/src/exercicio_01/Pessoa.java
@@ -0,0 +1,59 @@
+package exercicio_01;
+
+public class Pessoa {
+    // instancias
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    // construtores
+    public Pessoa(String nome, String endereco, String telefone) { // os tres atributos
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Pessoa(String nome, String telefone) { // apenas nome e telefone
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = "";
+    }
+
+    public Pessoa(String nome) { // apenas nome
+        this.nome = nome;
+        this.endereco = "";
+        this.telefone = "";
+    }
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    // metodos
+    public String toString() {
+        String sPessoa = "\n\tNome: " + this.nome + "\n";
+        if (this.endereco != "") {
+            sPessoa += "\tEndereco: " + this.endereco + "\n";
+        }
+        if (this.telefone != "") {
+            sPessoa += "\tTelefone: " + this.telefone + "\n";
+        }
+        return sPessoa;
+    }
+}
---
+++ heranca-exercicios/src/exercicio_02/Fornecedor.java
@@ -0,0 +1,52 @@
+package exercicio_02;
+import exercicio_01.Pessoa;
+
+public class Fornecedor extends Pessoa {
+    // instancias
+    private double valorCredito, valorDivida;
+
+    // construtores
+    public Fornecedor(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public Fornecedor(String nome, String endereco, String telefone) {
+        super(nome, endereco, telefone);
+        this.valorCredito = 0;
+        this.valorDivida = 0;
+    }
+
+    // construtor fornecedor com uma pessoa
+    public Fornecedor(Pessoa pessoa, double valorCredito, double valorDivida) {
+        super(pessoa.getNome(), pessoa.getEndereco(), pessoa.getTelefone());
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    // getters e setters
+    public double getValorCredito() {
+        return valorCredito;
+    }
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+    public double getValorDivida() {
+        return valorDivida;
+    }
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    // metodos
+    public double obterSaldo() {
+        return valorCredito - valorDivida;
+    }
+    public String toString() {
+        String sFornecedor = super.toString()
+                + "\tValor crédito: " + valorCredito
+                + "\n\tValor divida: " + valorDivida + "\n";
+        return sFornecedor;
+    }
+}
